42adb6db62f43d40f833b756cb1a5ec1d81f58bd
Gentjan Kolicaj <gentjankolicaj@yahoo.com>
2021-09-18 22:50:44 +0200

    section_4 new go files

--- section_3/helloworld.go
+++ section_3/helloworld.go
@@ -5,4 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world !!!")
 	fmt.Println("This is a go example")
+
+	foo()
 }
+
+func foo() {
+	fmt.Println("Called foo() function")
+}
+
+//Control flow
+//1.Sequence => Top to Bottom
+//2.Loop =>for , while
+//3.Conditional =>if ,else
