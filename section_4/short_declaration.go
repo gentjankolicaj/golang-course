61f4a8cd077429027c5af6a2f50e930690bd110c
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2021-09-19 19:34:21 +0200

    Update of short_declaration.go

--- section_4/short_declaration.go
+++ section_4/short_declaration.go
@@ -1,1 +1,41 @@
-package section_4
+//go:build ignore
+// +build ignore
+
+package main
+
+import "fmt"
+
+func main() {
+
+	BooleanType()
+	SignedIntegerType()
+	SignedDoubleType()
+	StringType()
+
+}
+
+func BooleanType() {
+	//Short declaration of boolean
+	flag := true
+	fmt.Println(flag)
+	flag = false
+	fmt.Println(flag)
+}
+
+func SignedIntegerType() {
+	//Short declaration
+	value := 10
+	fmt.Println(value)
+}
+
+func SignedDoubleType() {
+	//Short declaration
+	value := -2.1
+	fmt.Println(value)
+}
+
+func StringType() {
+	//Short declaration
+	str := "StringType"
+	fmt.Println(str)
+}
