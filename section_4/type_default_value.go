dd7acd63ffa7a2fe7d0d4f4afa1528b7218182dd
Gentjan Kolicaj <gentjankolicaj@yahoo.com>
2021-09-19 20:17:05 +0200

    type_default_value.go updated

--- section_4/type_default_value.go
+++ section_4/type_default_value.go
@@ -2,6 +2,42 @@
 
 package main
 
+import "fmt"
+
+var a bool
+
+var b uint
+
+var c string
+
+var d int
+
+var e float64
+
+var f interface{}
+
+var g byte  //uint8 2^8
+
 func main() {
 
+	fmt.Println(a)
+	fmt.Printf("%T \n",a)
+
+	fmt.Println(b)
+	fmt.Printf("%T \n",b)
+
+	fmt.Println(c)
+	fmt.Printf("%T \n",c)
+
+	fmt.Println(d)
+	fmt.Printf("%T \n",d)
+
+	fmt.Println(e)
+	fmt.Printf("%T \n",e)
+
+	fmt.Println(f)
+	fmt.Printf("%T \n",f)
+	
+	fmt.Println(g)
+	fmt.Printf("%T \n",g)
 }
--- section_4/type_primitive.go
+++ section_4/type_primitive.go
@@ -2,6 +2,27 @@
 
 package main
 
+import "fmt"
+
 func main() {
+
+	var a bool
+
+	var b uint
+
+	var c string
+
+	var d int
+
+	var e float64
+
+	var f interface{}
+
+	var g byte  //uint8 2^8
+
+	var i string
+
+	fmt.Println(a,b,c,d,e,f,g,i)
+
 
 }
