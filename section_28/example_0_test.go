3e7cb9b38f11ebc694681e6bbce43a62c3ec345f
Gentjan Kolicaj <gentjankolicaj@yahoo.com>
2021-10-22 11:44:22 +0200

    file name updates
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

---
+++ section_28/example_0_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("1,2,3,4,5 => sum = ", Sum(1, 2, 3, 4, 5))
+}
+
+func Sum(x ...int) int {
+	var sum int = 0
+	for _, v := range x {
+		sum = sum + v
+	}
+	return sum
+}
